16cad71825fbf3af44e703f1f19b94cac44dac8c
TIPPAWANKLBUDC-byte <TIPPAWAN.KL.BUDC@cpird.in.th>
2025-09-15 14:03:09 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbx20TBHxQWbVa_CkihUzNv8OCWO47XfRBMxiPfH4hWq53FGnRNK1gNgfGH5tuMYgOc/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyQZyjFygEUXo03C9lxWir4Cbfr2ESjTqZzitcc60GBv_ZbloZ0oUsyw3zWSf9e6xJphw/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzAmx9lCsrxVzDcMI9hKbzBHhVk_KiXcUh-JfN9mKYTlEShPfhMpLEGVIUrzKAcCk8jtQ/exec';
 //LIFF_ID
     const LIFF_ID = '2008088011-R93YNnny'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008088011-a4pzWXXw'; // LIFF ID history
     const LIFF_REGISTER = '2008088011-jrbZl99G'; // LIFF ID register
+
 
